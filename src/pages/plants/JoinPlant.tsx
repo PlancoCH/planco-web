587f160c916fc0a9e82884fa7dfb21f881c37d05
Nils Utiger <nils.utiger@suva.ch>
2026-06-18 16:37:47 +0200

    feat; add ui backbuttons to new pages

--- src/pages/plants/JoinPlant.tsx
+++ src/pages/plants/JoinPlant.tsx
@@ -1,9 +1,11 @@
 import PageContainer from "../../components/ui/PageContainer";
 import PageTitle from "../../components/ui/PageTitle";
+import BackButton from "../../components/ui/BackButton";
 
 export default function JoinPlant() {
   return (
     <PageContainer>
+    <BackButton to="/plants" text="Back to plants"/>
       <PageTitle
         title="Join a Plant"
         subtitle="View and join an existing plant from another user to start tracking its health and growth."
--- src/pages/plants/SharePlant.tsx
+++ src/pages/plants/SharePlant.tsx
@@ -1,9 +1,11 @@
+import BackButton from "../../components/ui/BackButton";
 import PageContainer from "../../components/ui/PageContainer";
 import PageTitle from "../../components/ui/PageTitle";
 
 export default function SharePlant() {
   return (
     <PageContainer>
+    <BackButton text="Back to plant"/>
       <PageTitle
         title="Share a Plant"
         subtitle="Invite another user to join and track an existing plant."
