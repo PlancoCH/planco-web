b31240d78d61f209252f288b3704eb1c18d410a3
Nils Utiger <nils.utiger@suva.ch>
2026-06-18 16:35:36 +0200

    feat; add nessacary pages

---
+++ src/pages/plants/JoinPlant.tsx
@@ -0,0 +1,14 @@
+import PageContainer from "../../components/ui/PageContainer";
+import PageTitle from "../../components/ui/PageTitle";
+
+export default function JoinPlant() {
+  return (
+    <PageContainer>
+      <PageTitle
+        title="Join a Plant"
+        subtitle="View and join an existing plant from another user to start tracking its health and growth."
+      />
+
+    </PageContainer>
+  );
+}
---
+++ src/pages/plants/SharePlant.tsx
@@ -0,0 +1,13 @@
+import PageContainer from "../../components/ui/PageContainer";
+import PageTitle from "../../components/ui/PageTitle";
+
+export default function SharePlant() {
+  return (
+    <PageContainer>
+      <PageTitle
+        title="Share a Plant"
+        subtitle="Invite another user to join and track an existing plant."
+      />
+    </PageContainer>
+  );
+}
